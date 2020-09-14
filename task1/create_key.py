990f443a1fa4062e87eaab15160f9615a12d02e0
SN4KEBYTE <timurcat7@gmail.com>
2020-09-15 00:29:05 +0700

    Added typehints.

--- task1/create_key.py
+++ task1/create_key.py
@@ -5,8 +5,8 @@ from task1.utils import swap
 
 def create_key(s: str, file) -> None:
     for i in range(len(s) - 1, 0, -1):
-        j = randint(0, i)
-        s = swap(s, i, j)
+        j: int = randint(0, i)
+        s: str = swap(s, i, j)
 
     with open(file, 'w', encoding='utf-8') as out:
         out.write(s)
