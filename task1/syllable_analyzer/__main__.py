ba2b672a3c70eae8c9373baca7e3f782dc853bb2
SN4KEBYTE <timurcat7@gmail.com>
2020-09-15 00:28:46 +0700

    Init commit.

---
+++ task1/syllable_analyzer/__main__.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from pathlib import Path
+
+from task1.const import ALPHABET
+
+WORK_DIR = Path().absolute() / 'task1' / 'syllable_analyzer'
+SYLLABLE_LEN = 2
+
+
+def preprocess(text: str) -> str:
+    return ''.join(ch for ch in text.lower() if ch in ALPHABET)
+
+
+def main():
+    with open(WORK_DIR / 'data' / 'text.txt', 'r', encoding='utf-8') as inp:
+        text: str = preprocess(inp.read())
+
+    c = Counter([text[i:i+SYLLABLE_LEN] for i in range(0, len(text), SYLLABLE_LEN)])
+
+    with open(WORK_DIR / 'data' / 'result.txt', 'w', encoding='utf-8') as out:
+        for syllable, frequency in c.most_common():
+            out.write(f'{syllable} {frequency}\n')
+
+
+if __name__ == '__main__':
+    main()
