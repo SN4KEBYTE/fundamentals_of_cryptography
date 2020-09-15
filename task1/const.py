c84041c85b65487f47258383e0823bc13a47d757
Касимов Тимур Рустамович <kasimov.2018@stud.nstu.ru>
2020-09-15 09:41:50 +0700

    Fixed work directory.

--- task1/const.py
+++ task1/const.py
@@ -3,6 +3,7 @@ from pathlib import Path
 ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 MOST_FREQUENT = 'оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфъё'
 
-WORK_DIR = Path().absolute() / 'task1' / 'p1'
+WORK_DIR = Path(__file__).parent.absolute()
+
 RAW_TEXTS_DIR = WORK_DIR / 'raw'
 ENC_TEXTS_DIR = WORK_DIR / 'encrypted'
