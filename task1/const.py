a91e63c2fadd01819c3ac2f097bb543913a51056
SN4KEBYTE <timurcat7@gmail.com>
2020-09-15 20:02:38 +0700

    Added global syllable len. Fixed work directory detection.

--- task1/const.py
+++ task1/const.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-MOST_FREQUENT = 'оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфъё'
 
-WORK_DIR = Path(__file__).parent.absolute()
+SYLLABLE_LEN = 2
 
+WORK_DIR = Path(__file__).parent.absolute() / 'simple_sub' / 'data'
 RAW_TEXTS_DIR = WORK_DIR / 'raw'
 ENC_TEXTS_DIR = WORK_DIR / 'encrypted'
