daf329a8319190a5e5ab059002f82abb3531baa6
SN4KEBYTE <timurcat7@gmail.com>
2020-09-15 20:02:06 +0700

    Added file output.

--- task1/create_key.py
+++ task1/create_key.py
@@ -1,10 +1,10 @@
 from random import randint
 
-from task1.types import Path
+from task1.types import PathType
 from task1.utils import swap
 
 
-def create_key(alphabet: str, out_path: Path) -> None:
+def create_key(alphabet: str, out_path: PathType) -> None:
     for i in range(len(alphabet) - 1, 0, -1):
         j: int = randint(0, i)
         alphabet: str = swap(alphabet, i, j)
