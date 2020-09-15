ca0765aeba08eac9126287d1aa78338aa4273129
SN4KEBYTE <timurcat7@gmail.com>
2020-09-15 19:15:12 +0700

    Now class works only with files. Added key validation.

--- task1/simple_sub/simple_substitution.py
+++ task1/simple_sub/simple_substitution.py
@@ -1,55 +1,56 @@
+from collections import Counter
+from pprint import pprint
+from typing import Optional
+
 from task1.const import ALPHABET, MOST_FREQUENT
 from task1.types import Path
-from typing import Optional
-from pprint import pprint
-from collections import Counter, OrderedDict
+from task1.utils import preprocess
 
 
 class SimpleSubstitutionCipher:
-    def __init__(self, key: Optional[str] = None, key_path: Optional[Path] = None) -> None:
-        if key is not None and key_path is not None:
-            raise RuntimeError('You can not use both key and path to key file.')
+    def __init__(self, key_path: Path) -> None:
+        k = self.__read_key(key_path)
 
-        self.__key: str = key if key is not None else self.__read_key(key_path)
+        if not self.__validate_key(k):
+            raise RuntimeError('Key must contain only cyrillic letters. Each one must occur no more than once.')
 
-    def encrypt(self, text_path: Path, out_path: Path = None) -> Optional[str]:
+        self.__key: str = k
+
+    def encrypt(self, text_path: Path, out_path: Path) -> None:
         mapping = {k: v for k, v in zip(ALPHABET, self.__key)}
 
         with open(text_path, 'r', encoding='utf-8') as inp:
-            text = self.__preprocess(inp.read())
+            text = preprocess(inp.read())
 
         encrypted: str = ''
 
         for ch in text:
             encrypted += mapping[ch]
 
-        if out_path is None:
-            return encrypted
-
         with open(out_path, 'w', encoding='utf-8') as out:
             out.write(encrypted)
 
     @staticmethod
-    def decrypt(text_path: Path, out_path: Path = None) -> Optional[str]:
+    def decrypt(text_path: Path, out_path: Path) -> None:
         with open(text_path, 'r', encoding='utf-8') as inp:
             encrypted = inp.read()
 
         c = Counter(encrypted)
         pprint(c.most_common())
 
-        mapping = {k[0]: v for k, v in zip(c.most_common(), MOST_FREQUENT)}
-        pprint(mapping)
-
-        decrypted: str = ''
-
-        for ch in encrypted:
-            decrypted += mapping[ch] if ch in ALPHABET else ch
-
-        if out_path is None:
-            return decrypted
-
-        with open(out_path, 'w', encoding='utf-8') as out:
-            out.write(decrypted)
+        # mapping = {k[0]: v for k, v in zip(c.most_common(), MOST_FREQUENT)}
+        # pprint(mapping)
+        #
+        # decrypted: str = ''
+        #
+        # for ch in encrypted:
+        #     decrypted += mapping[ch] if ch in ALPHABET else ch
+        #
+        # if out_path is None:
+        #     return decrypted
+        #
+        # with open(out_path, 'w', encoding='utf-8') as out:
+        #     out.write(decrypted)
 
     @staticmethod
     def __read_key(path: Path) -> str:
@@ -57,8 +58,8 @@ class SimpleSubstitutionCipher:
             return inp.read()
 
     @staticmethod
-    def __preprocess(text: str) -> str:
-        return ''.join(ch for ch in text.lower() if ch in ALPHABET)
+    def __validate_key(key: str) -> bool:
+        return sorted(key) == sorted(ALPHABET)
 
     @property
     def key(self):
@@ -66,7 +67,7 @@ class SimpleSubstitutionCipher:
 
     @key.setter
     def key(self, value):
-        if sorted(value) != sorted(ALPHABET):
+        if not self.__validate_key(value):
             raise RuntimeError('Key must contain only cyrillic letters. Each one must occur no more than once.')
 
         self.__key = value
