bc1612865eb348991235eeec29d6ec9d57742152
Касимов Тимур Рустамович <kasimov.2018@stud.nstu.ru>
2020-09-15 09:42:48 +0700

    Preprocess func was moved here.

--- task1/utils.py
+++ task1/utils.py
@@ -1,5 +1,12 @@
+from task1.const import ALPHABET
+
+
 def swap(s: str, i: int, j: int) -> str:
     str_list = list(s)
     str_list[i], str_list[j] = str_list[j], str_list[i]
 
     return ''.join(str_list)
+
+
+def preprocess(text: str) -> str:
+    return ''.join(ch for ch in text.lower() if ch in ALPHABET)
